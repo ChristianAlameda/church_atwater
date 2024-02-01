729a4b8f90fb528da64ece128cca72a97989bd3d
ChristianAlameda <jazzbilbo@gmail.com>
2024-01-31 21:42:54 -0800

    added more pages, need to fill them out

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//SLIDESHOW1: index.html
+
+// button reference to website: ALL
+function navigateToPage1(url) {
+    window.location.href = url;
+}
+
+// button reference to html: index.html
+function navigateToPage2(url) {
+    window.location.href = url;
+}
+
+function navigateToPage3(url) {
+    window.location.href = url;
+}
+
+
+
